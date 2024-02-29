3cb783af33eaf22818d9b2e35ff54179b48158f8
vasanthivgvr <139816564+vasanthivgvr@users.noreply.github.com>
2024-02-29 22:12:20 +0530

    Add files via upload

---
+++ src/result_management_portal/Result_management_portal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package result_management_portal;
+
+/**
+ *
+ * @author MSI
+ */
+public class Result_management_portal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
